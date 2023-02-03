015dc262b3a078902234db9c5ad6729016f4ebb8
phidoanUFL <phidoan@ufl.edu>
2023-02-02 18:25:37 -0600

    simple login page

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
+
