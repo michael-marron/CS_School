9504567c7b3b8e949f13f113099d4d119f3a7856
jbarber877 <80484728+jbarber877@users.noreply.github.com>
2023-02-03 10:11:50 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,19 @@
-# for testing
+# This file is for setting up the tests
+
+import pytest
+from project import create_app, db
+
+@pytest.fixture()
+def app():
+  app = create_app("sqlite://") # create an app with a temporary sqlite db
+  with app.app_context():
+    # create the tables in the DB
+    db.create_all()
+  
+  # code after this line will be ran once tests are finished
+  yield app
+  
+@pytest.fixture()
+def client(app):
+  # is used to simulate requests to the client
+  return app.test_client()
