23e0e9f525237d48d67cc54f18f7a11c3a33add9
Michael Marron <62182318+michael-marron@users.noreply.github.com>
2023-02-06 09:17:42 -0500

    test 3

--- tutor_service/routes.py
+++ tutor_service/routes.py
@@ -6,10 +6,6 @@ from flask import render_template, redirect, url_for, flash
 def home_page():
     return render_template('home.html')
 
-@app.route('/login')
-def login_page():
-    return render_template('home.html')
-
 @app.route('test')
 def test_page():
     return render_template('test.html')    
