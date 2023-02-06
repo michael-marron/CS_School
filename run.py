ab8195c2668e2b46e6ef7d852a2be666065f2295
Daniela Villalva <dvillalva@ufl.edu>
2023-02-06 11:57:45 -0500

    attempting to transfer files

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from tutor_service import app
 
+@app.route("/tutor-page", methods=["POST"])
+def tutor_page():
+    return render_template(
+        "tutor-page.html",
+    )
+
 if __name__ == '__main__':
     app.run(debug=True)
