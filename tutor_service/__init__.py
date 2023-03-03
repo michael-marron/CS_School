cf42542af4a510b598d3ede351fc7354f6b14f83
jbarber877 <80484728+jbarber877@users.noreply.github.com>
2023-03-03 15:15:24 -0500

    Update __init__.py

--- tutor_service/__init__.py
+++ tutor_service/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from .extensions import mail
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -15,6 +15,19 @@ def create_app(config_file=None):
     app.config['SECRET_KEY'] = "secret"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
+    # mail routes
+    app.add_url_rule("/foo", view_func=routes.foo)
+    app.add_url_rule("/email", view_func=routes.email, methods = methods)
+
+    # set up mail server - if you want your own account, change username / password
+    app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+    app.config['MAIL_PORT'] = 2525
+    app.config['MAIL_USERNAME'] = '15948b25a40464'
+    app.config['MAIL_PASSWORD'] = '3791137de16d49'
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
+    
+    mail.init_app(app)
     
 
 
