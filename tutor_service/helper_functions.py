401c40cf549f922fcbfa60e6026fb57cb75f3ef2
dvillalva00 <74994747+dvillalva00@users.noreply.github.com>
2023-03-03 12:38:08 -0500

    Update helper_functions.py

--- tutor_service/helper_functions.py
+++ tutor_service/helper_functions.py
@@ -3,24 +3,40 @@ from datetime import datetime, timedelta, date
 
 def get_time_list(start, end):
     list_times = []
-    
+    #start time is 8:00am
+    #end time is 21:00pm -> 9:00pm
+    #step of 1
     for x in range(start, end, 1):
         start_time = ""
         end_time = ""
         start_end_time = []
 
-        if x <= 12:
-            val = x
-            start_time = f"{val}:00AM - {val}:25AM"
-            end_time = f"{val}:30AM - {val}:55AM"
-        else:
-            val = x - 12
-            start_time = f"{val}:00PM - {val}:25PM"
-            end_time = f"{val}:30PM - {val}:55PM"
+        for count in range(2):
+            if x <= 12:
+                val = x
+                if count == 0:
+                    start_time = f"{val}:00AM"
+                    end_time = f"{val}:25AM"
 
-        start_end_time.append(start_time)
-        start_end_time.append(end_time)
-        list_times.append(start_end_time)
+                else:
+                    start_time = f"{val}:30AM"
+                    end_time = f"{val}:55AM"
+
+            else:
+                val = x - 12
+                if count == 0:
+                    start_time = f"{val}:00PM"
+                    end_time = f"{val}:25PM"
+
+                else:
+                    start_time = f"{val}:30PM"
+                    end_time = f"{val}:55PM"
+
+            start_end_time.clear()
+            start_end_time.append(start_time)
+            start_end_time.append(end_time)
+            list_times.append(start_end_time[:])
+
 
     return list_times
 
@@ -37,3 +53,4 @@ def get_weekdays():
         dates_list.append(day_string)
 
     return dates_list    
+
