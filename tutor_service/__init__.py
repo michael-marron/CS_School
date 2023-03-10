39e7270adfa45e22ec3d9f4b20f075498023dcf9
phidoanUFL <phidoan@ufl.edu>
2023-03-10 12:14:50 -0600

    adding login, reg, and reset to the main proj, reset not implemented

--- tutor_service/__init__.py
+++ tutor_service/__init__.py
@@ -7,16 +7,17 @@ def create_app(config_file=None):
     # create and configure the app
     app = Flask(__name__)
 
+    app.secret_key = 'sosecret'
     app.add_url_rule("/home", view_func=routes.home_page)
     app.add_url_rule("/", view_func=routes.calendar_page)
     app.add_url_rule("/calendar", view_func=routes.calendar_page)
     app.add_url_rule("/test", view_func=routes.home_page)
+    app.add_url_rule("/log", view_func=routes.login, methods=['GET','POST'])
+    app.add_url_rule("/reg", view_func=routes.register, methods=['GET','POST'])
+    app.add_url_rule("/reset", view_func=routes.reset, methods=['GET','POST'])
 
     app.config['SECRET_KEY'] = "secret"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-
-    
-
 
     return app
 
